8bc2db6ba72576ba367975503c4faa9dba5d4e12
whatasoda <git@whatasoda.me>
2021-07-02 00:02:26 +0900

    fix: set base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   root: path.resolve(__dirname, 'src'),
+  base: process.env.NODE_ENV === 'production' ? 'https://whatasoda.github.io/steam-thumbnail-collector/' : '/',
   build: {
     outDir: path.resolve(__dirname, 'dist'),
   },
