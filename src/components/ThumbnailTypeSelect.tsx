cc54c0037557438601c87376978d6f79a079fe62
whatasoda <git@whatasoda.me>
2021-07-02 22:34:58 +0900

    feat: add ThumbnailTypeSelect

---
+++ src/components/ThumbnailTypeSelect.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ImageFetchMode } from '../fetch-image';
+
+const items: { value: ImageFetchMode; label: string }[] = [
+  { value: 'capsule', label: '1. 一番小さい 横長' },
+  { value: 'header', label: '2. 少し大きめ 横長' },
+  { value: 'library', label: '3. 大きめ 縦長' },
+  { value: 'library-fallback', label: '3 → 2 → 1 の順で画像があるものを使う' },
+  { value: 'header-fallback', label: '2 → 1 の順で画像があるものを使う' },
+];
+
+interface ThumbnailTypeSelectProps {
+  value: ImageFetchMode;
+  onChange: (mode: ImageFetchMode) => void;
+}
+
+export default function ThumbnailTypeSelect({ value, onChange }: ThumbnailTypeSelectProps) {
+  return (
+    <StyledSelect
+      value={value}
+      onChange={(event) => {
+        onChange(event.currentTarget.value as ImageFetchMode);
+      }}
+    >
+      {items.map(({ value, label }) => (
+        <option key={value} value={value} label={label} />
+      ))}
+    </StyledSelect>
+  );
+}
+
+const StyledSelect = styled.select``;
